ee179c95be5eb070e20cdd9de92b5fd883b6c3cd
costagavras <cgavras@yahoo.ca>
2018-10-03 17:00:20 -0400

    new review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,17 @@
 class ReviewsController < ApplicationController
+
   def create
+    @review = Review.new
+    @product = Product.find(params[:product_id])
+    @review.product_id = params[:product_id]
+    @review.description = params[:review][:description]
+    if @review.save
+      flash[:notice] = "Review saved !"
+      redirect_to "/products/#{@review.product_id}"
+    else
+      @product = Product.find(params[:product_id])
+      render "products/show"
+    end
   end
 
   def edit
@@ -10,4 +22,7 @@ class ReviewsController < ApplicationController
 
   def destroy
   end
+
+
+
 end
