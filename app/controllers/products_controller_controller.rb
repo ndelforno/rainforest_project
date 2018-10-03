cd2509fb22673a8efff404d6ca3c4e010e299f40
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-10-03 10:56:09 -0400

    index file created...

---
+++ app/controllers/products_controller_controller.rb
@@ -0,0 +1,20 @@
+class ProductsControllerController < ApplicationController
+  def index
+
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'products_controller/new'
+  get 'products_controller/create'
+  get 'products_controller/update'
+  get 'products_controller/edit'
+  get 'products_controller/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  resources :products
 end
