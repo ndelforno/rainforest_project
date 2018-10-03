a8618b357dd078a2625d6d9bc89c59aeb3222aa4
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-10-03 15:24:24 -0400

    reviews routes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :product
+
+    validates :description, presence: { message: "field must be filled out!" }
+    validates :product_id, presence: { message: "field must be filled out!" }
+
+
 end
