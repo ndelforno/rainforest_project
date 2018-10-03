104d47419a5a17408a6ed2d5c222d6ede945d77d
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-10-03 13:49:33 -0400

    validation done for the table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price_in_cents, presence: true
+  validates :price_in_cents, numericality: {only_integer: true}
 end
