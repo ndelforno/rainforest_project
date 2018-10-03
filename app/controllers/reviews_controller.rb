56b4f9a619cb9ee44b3f968321dce0969470dad6
Ndelforno <nicolas.delforno@gmail.com>
2018-10-03 18:11:13 -0400

    edit and create review done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,9 +15,21 @@ class ReviewsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
   end
 
   def update
+    @review = Review.find(params[:id])
+    @review.product_id = params[:product_id]
+    @review.description = params[:review][:description]
+    if @review.save
+      flash[:notice] = "Review updated !"
+      redirect_to "/products/#{@review.product_id}"
+    else
+      @product = Product.find(params[:product_id])
+      render "products/show"
+    end
   end
 
   def destroy
