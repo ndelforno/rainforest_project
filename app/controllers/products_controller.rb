40db2d83891d741c518ba2db4a6f1137a049920d
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-10-03 14:09:37 -0400

    new item form and save

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,21 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+    if @product.save
+
+       redirect_to '/products'
+     else
+       render :new
+    end
+
   end
 
   def update
